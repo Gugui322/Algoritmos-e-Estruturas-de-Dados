f0ddeff9c7b3b1f2b9d5c86253f6c530962828dc
Gugui322 <65572156+Gugui322@users.noreply.github.com>
2022-03-03 22:03:29 -0300

    quick fix

--- src/pascal/fatorial-recusiva.pas
+++ src/pascal/fatorial-recusiva.pas
@@ -1,26 +1,32 @@
-program fatorial_recursiva;
-var
-	num : longint;
-
-function fatorial (number : longint): longint;
-begin
-    if (number = 1) then
-        fatorial := 1
-    else 
-        fatorial := number * fatorial(number-1)
-end;
-
-function fatorialb(n:integer): integer;
-begin
-	if n > 1 then
-		fatorial := fatorial(n-1) * n
-	else fatorial := 1;
-end;
-
-begin
+PROGRAM fatorial_recursiva;
+//old "buggy" version
+//function fatorial (number : longint): longint;
+//begin
+//    if (number = 1) then
+//       fatorial := 1
+//    else 
+//        fatorial := number * fatorial(number-1)
+//end;
+{begin
     writeln('Informe um numero');
     readln(num);
     writeln(' ');
-
-    writeln(num,'! = ',fatorial(num));
-end.
+    writeln(num,'! = ',fatorial(num))
+end}
+VAR N : LONGINT;
+FUNCTION FACTORIAL(N: LONGINT): LONGINT;
+(* RECURSIVE COMPUTATION OF N FACTORIAL *)
+BEGIN
+  (* TEST FOR STOPPING STATE *)
+  IF N <= 0 THEN
+    FACTORIAL := 1;
+  ELSE
+    FACTORIAL := N * FACTORIAL(N - 1);
+END;(* FACTORIAL *)
+WRITELN('Digite um numero');
+READLN(N);
+WRITELN(N, '! = ', FACTORIAL);
+READLN();
+END.
+//should compile on freepascal 3.2.2 without errors now.
+// 03/03/2022
